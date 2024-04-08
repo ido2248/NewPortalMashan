bf0031dac2c5c14973eea83901c6dd4d62135f21
ido <ido240404@gmail.com>
2024-04-08 22:04:54 +0300

    update

--- src/components/PopUpPdf.jsx
+++ src/components/PopUpPdf.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Document, Page } from 'react-pdf'
 
 const PopUpPdf = ({isOpen, onClose }) => {
   if(!isOpen){
@@ -11,10 +12,12 @@ const PopUpPdf = ({isOpen, onClose }) => {
     }
   }
   return (
-    <div className=' fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50' onClick={closepopup}>
+    <div className=' fixed inset-0 flex items-center justify-center z-50 bg-slate-800 bg-opacity-90' onClick={closepopup}>
         <div className='bg-white p-6 rounded shadow-lg'> 
-            <h1>sup</h1>
-            <button onClick={onClose}></button>
+          <Document file='/pdf/tash1.pdf'>
+            <Page  pageNumber={2}></Page>
+          </Document>
+          <button onClick={onClose}></button>
         </div>
     </div>
   )
